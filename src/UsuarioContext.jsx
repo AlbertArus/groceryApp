ad18d38b51a3147fad6d9103a6ee745481a1208e
Albert Arus <albert.arusg@gmail.com>
2024-11-03 23:17:17 +0100

    fix usuario - pending cargar toda info al principio

--- src/UsuarioContext.jsx
+++ src/UsuarioContext.jsx
@@ -9,28 +9,58 @@ export const useUsuario = () => useContext(UsuarioContext);
 
 export const UsuarioProvider = ({ children }) => {
   const [usuario, setUsuario] = useState(undefined);
+  // eslint-disable-next-line
+  const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChanged(auth, async (usuario) => {
+  //     if (usuario) {
+  //       setUsuario(usuario);
+  //       // Cargar la información del usuario desde Firestore
+  //       const docRef = doc(db, "usuarios", usuario.uid);
+  //       const docSnap = await getDoc(docRef);
+  //       if (docSnap.exists()) {
+  //           const userData = docSnap.data();
+  //           // Aquí puedes almacenar los datos en el estado si lo deseas
+  //           setUsuario({ ...usuario, ...userData });
+  //           // loadListasFromFirebase();
+  //           } else {
+  //           console.log("No hay tal documento!");
+  //       }
+  //     } else {
+  //       setUsuario(null);
+  //     }
+  //   });
+  //   return () => unsubscribe();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (usuario) => {
-      if (usuario) {
-        setUsuario(usuario);
-        // Cargar la información del usuario desde Firestore
-        const docRef = doc(db, "usuarios", usuario.uid);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
+    const unsubscribe = onAuthStateChanged(auth, async (authUser) => {
+      try {
+        if (authUser) {
+          // Cargar la información del usuario desde Firestore
+          const docRef = doc(db, "usuarios", authUser.uid);
+          const docSnap = await getDoc(docRef);
+          
+          if (docSnap.exists()) {
             const userData = docSnap.data();
-            // Aquí puedes almacenar los datos en el estado si lo deseas
-            setUsuario({ ...usuario, ...userData });
-            // loadListasFromFirebase();
-            } else {
-            console.log("No hay tal documento!");
+            setUsuario({ ...authUser, ...userData });
+          } else {
+            setUsuario(authUser);
+            console.log("No hay información adicional del usuario");
+          }
+        } else {
+          setUsuario(null);
         }
-      } else {
-        setUsuario(null);
+      } catch (error) {
+        console.error("Error al cargar datos del usuario:", error);
+      } finally {
+        setLoading(false);
       }
     });
+  
     return () => unsubscribe();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
