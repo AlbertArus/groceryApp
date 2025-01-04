785bf28d756e9caa7262455c33f4d38429af90d0
Albert Arus <albert.arusg@gmail.com>
2025-01-04 20:53:39 +0100

    fix buttonArea cálculo scroll (315)

--- src/ui-components/ButtonArea.jsx
+++ src/ui-components/ButtonArea.jsx
@@ -1,36 +1,13 @@
-import { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 
 const ButtonArea = ({ buttonCopy, onClick, style, children }) => {
-    const wrapperRef = useRef(null);
-    const contentRef = useRef(null);
-    const [isScrollable, setIsScrollable] = useState(false);
-
-    useEffect(() => {
-        const checkScrollability = () => {
-            const contentElement = contentRef.current;
-            const wrapperElement = wrapperRef.current;
-
-            if (contentElement && wrapperElement) {
-                const isContentTaller = contentElement.scrollHeight > wrapperElement.clientHeight;
-                setIsScrollable(isContentTaller);
-            }
-        };
-
-        checkScrollability();
-        window.addEventListener('resize', checkScrollability);
-
-        return () => {
-            window.removeEventListener('resize', checkScrollability);
-        };
-    }, [children]);
 
     return (
-        <div ref={wrapperRef} className="wrapper">
-            <div ref={contentRef} className="content" style={{overflowY: isScrollable ? 'auto' : 'hidden'}}>
+        <div className="wrapper">
+            <div className="content">
                 {children}
             </div>   
-            <div className="button-main-sticky" style={{position: "sticky", bottom: "0"}}>
+            <div className="button-main-sticky">
                 <Button
                     buttonCopy={buttonCopy}
                     onClick={onClick}
