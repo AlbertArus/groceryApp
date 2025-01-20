1bf62cbeb0bcea810bf249eddbf41b76ea33cd6b
Albert Arus <albert.arusg@gmail.com>
2025-01-20 21:49:42 +0100

    manifest

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -4,7 +4,7 @@ const LoadingPage = () => {
   return (
     <div className="LoadingPage">
       <div className='LoadingPageContent'>
-        <img src='/Fotos GroceryApp/favicon/android-chrome-512x512.png' alt='Cargando página' style={{width: "33%", marginBottom: "20px"}}/>
+        <img src='/Fotos GroceryApp/favicon/android/android-launchericon-512-512.png' alt='Cargando página' style={{width: "33%", marginBottom: "20px"}}/>
         <h2 style={{fontWeight: "500"}}>Estamos preparando tu app...</h2>
       </div>
     </div>
