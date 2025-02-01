da031443847e0c4d36845db282dc6d11e3b8a176
Albert Arus <albert.arusg@gmail.com>
2025-02-01 23:22:55 +0100

    variables de entorno (221)

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,19 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
-const firebaseConfig = {
-    apiKey: "AIzaSyC6pqrzGdvCgL5WnzW8Ay0mzP5-YYFt7u8",
-    authDomain: "groceryapp-7462d.firebaseapp.com",
-    // databaseURL: "https://groceryapp-7462d-default-rtdb.europe-west1.firebasedatabase.app/",
-    projectId: "groceryapp-7462d",
-    storageBucket: "groceryapp-7462d.appspot.com",
-    messagingSenderId: "567726198663",
-    appId: "1:567726198663:web:cba7b4fa9c6595ed3acd22",
-    measurementId: "G-DQMEE49WTB"
-}
+let firebaseConfig = {}
+
+if (process.env.REACT_APP_ENV === 'production') {
+    firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG_PROD);
+  } else {
+    firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG_DEV);
+  }
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 
-export default app; 
+export default app;
