5281ce23f489bf323ed38d956df9a0dc1eabb330
Albert Arus <albert.arusg@gmail.com>
2025-02-06 21:15:19 +0100

    button icono para abrir cámara

---
+++ src/ui-components/Camera.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import CameraOrGallery from '../functions/CameraOrGallery';
+import OCR from '../OCR/OCR';
+
+const Camera = () => {
+    const [cameraOpen, setCameraOpen] = useState(false);
+    const [image, setImage] = useState(null);
+
+    const handleImageCapture = (image) => {
+        setImage(image)
+        setCameraOpen(false)
+    }
+
+    if (cameraOpen) {
+        return <CameraOrGallery 
+            image={image}
+            setImage={setImage}
+            onClose={handleImageCapture}
+        />;
+    }
+
+    return (
+        <>
+            <div className="search-container" style={{ padding: "5px 5px 0px 5px", marginLeft: "10px" }}>
+                <span className="material-symbols-outlined" onClick={() => setCameraOpen(true)}>add_a_photo</span>
+            </div>
+            {image && 
+                <OCR 
+                    image={image}
+                    setImage={setImage}
+                />
+            }
+        </>
+    )
+}
+
+export default Camera
