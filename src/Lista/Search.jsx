8bccdec72d5af90b7abf24047d7da94e4640d266
Albert Arus <albert.arusg@gmail.com>
2025-02-06 20:58:33 +0100

    botones icono para abrir cámara (318)

--- src/Lista/Search.jsx
+++ src/Lista/Search.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react"
-import CameraOrGallery from "../functions/CameraOrGallery";
+import Camera from "../ui-components/Camera";
 // import ModalSheet from "../ui-components/ModalSheet";
 // import TabItemMenu from "../components/TabItemMenu";
 
@@ -8,7 +8,6 @@ const Search = ({setSearchResult }) => {
     // const [open, setOpen] = useState(false)
     // const [open, setOpen] = useState(false)
     const searchValueRef = useRef(null)
-    const [cameraOpen, setCameraOpen] = useState(false);
 
     useEffect(() => {
         const handleSearchFocus = (event) => {
@@ -34,10 +33,6 @@ const Search = ({setSearchResult }) => {
         searchValueRef.current.focus()
     }
 
-    if (cameraOpen) {
-        return <CameraOrGallery onClose={() => setCameraOpen(false)} />;
-    }
-
     return (
         <div className="search fila-between">
             <div className="search-container fila-start" style={{width: "100%"}}>
@@ -45,9 +40,7 @@ const Search = ({setSearchResult }) => {
                 <span className="material-symbols-outlined" style={{marginLeft: "8px", display: !focused ? "flex" : "none"}} onClick={handleSearch}>search</span>
                 <span className="material-symbols-outlined" style={{marginLeft: "8px", display: focused ? "flex" : "none"}} onClick={handleDeleteSearch}>close</span>
             </div>
-            <div className="search-container" style={{ padding: "5px 5px 0px 5px", marginLeft: "10px" }}>
-                <span className="material-symbols-outlined" onClick={() => setCameraOpen(true)}>add_a_photo</span>
-            </div>
+            <Camera />
             {/* <div className="search-container" style={{padding: "5px 5px 0px 5px", marginLeft:"10px"}}>
                 <span className="material-symbols-outlined" onClick={() => setOpen(true)}>filter_list</span>
             </div>
--- src/ui-components/Toggle.jsx
+++ src/ui-components/Toggle.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useUsuario } from "../UsuarioContext"
 import { useSearchParams } from "react-router-dom"
 import Camera from "./Camera"
@@ -6,7 +6,6 @@ import Camera from "./Camera"
 const Toggle = ({form, option1, option2, option3, origin, listas, lista, setFilteredListas, setFilteredListaForItems, isToggleActive, setIsToggleActive, isToggleSelected, setIsToggleSelected, setSearchParams, isScrolled, setIsScrolled }) => {
     const {usuario} = useUsuario()
     const [searchParams] = useSearchParams()
-    const [cameraOpen, setCameraOpen] = useState(false);
 
     const handleClickActive = (toggle) => {
         setIsToggleActive(toggle)
