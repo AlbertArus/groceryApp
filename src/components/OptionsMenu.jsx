4e94ae1245a8a66d73e45b5923157425fe648afc
Albert Arus <albert.arusg@gmail.com>
2025-03-12 21:12:08 +0100

    ocultar campos de lista como votos, precios si estás en pagos (368)

--- src/components/OptionsMenu.jsx
+++ src/components/OptionsMenu.jsx
@@ -1,52 +1,65 @@
-import { forwardRef } from "react"
+import { useState, useEffect, forwardRef } from "react"
 import ItemMenu from "./ItemMenu"
 import { useNavigate } from "react-router-dom"
 import { useUsuario } from '../UsuarioContext';
+import {useSearchParams} from "react-router-dom"
 
 const OptionsMenu = forwardRef(({ deleteLista, itemslength, lista, handleArchive, handleShowVotes, handleShowPrices, handleCheckAll, handleUnCheckAll, updateLista, style }, ref) => {
   const navigate = useNavigate()
   const { usuario } = useUsuario();
+  const [inPagos, setInPagos] = useState (false)
+  const [searchParams] = useSearchParams() // Es [] porque realmente es un state pero como solo leo y no actualizo valores, no tengo el setSearchParams (que el nombre es libre)
+
+  useEffect(() => {
+      if(searchParams.get("view") === "payments") {
+        setInPagos(true)
+      }
+  },[searchParams])
 
   return (
     <div className="optionsMenu" ref={ref} style={style}>
-      <ItemMenu
-        iconName={`${lista.userConfig?.[usuario.uid]?.showVotes ? "visibility_off" : "visibility"}`}
-        itemMenuName={`${lista.userConfig?.[usuario.uid]?.showVotes ? "Ocultar votaciones" : "Mostrar votaciones"}`}
-        onClick={() => handleShowVotes(lista)}
-        style={{display: itemslength < 2 ? "none" : "flex"}}
+        {!inPagos && (
+            <>
+            <ItemMenu
+              iconName={`${lista.userConfig?.[usuario.uid]?.showVotes ? "visibility_off" : "visibility"}`}
+              itemMenuName={`${lista.userConfig?.[usuario.uid]?.showVotes ? "Ocultar votaciones" : "Mostrar votaciones"}`}
+              onClick={() => handleShowVotes(lista)}
+              style={{display: itemslength < 2 ? "none" : "flex"}}
+              />
+            <ItemMenu
+              iconName={"euro_symbol"}
+              itemMenuName={`${lista.userConfig?.[usuario.uid]?.showPrices ? "Ocultar precios" : "Mostrar precios"}`}
+              onClick={() => handleShowPrices(lista)}
+              />
+            <ItemMenu
+              iconName={"check_box"}
+              itemMenuName={"Completar todo"}
+              onClick={handleCheckAll}
+              style={{display: itemslength < 2 ? "none" : "flex"}}
+              />
+            <ItemMenu
+              iconName={"check_box_outline_blank"}
+              itemMenuName={"Desmarcar todo"}
+              onClick={handleUnCheckAll}
+              style={{display: itemslength < 2 ? "none" : "flex"}}
+              />
+            </>
+        )}
+        <ItemMenu
+            iconName={lista.userConfig[usuario.uid].isArchived ? "unarchive" : "archive"}
+            itemMenuName={lista.userConfig[usuario.uid].isArchived ? "Desarchivar lista" : "Archivar lista"}
+            onClick={() => {handleArchive(lista);  navigate("/")}}
         />
-      <ItemMenu
-        iconName={"euro_symbol"}
-        itemMenuName={`${lista.userConfig?.[usuario.uid]?.showPrices ? "Ocultar precios" : "Mostrar precios"}`}
-        onClick={() => handleShowPrices(lista)}
+        <ItemMenu
+            iconName={"edit"}
+            itemMenuName={"Editar lista"}
+            onClick={() => navigate(`/newlist?lista=${lista.id}`)}
         />
-      <ItemMenu
-        iconName={"check_box"}
-        itemMenuName={"Completar todo"}
-        onClick={handleCheckAll}
-        style={{display: itemslength < 2 ? "none" : "flex"}}
+        <ItemMenu
+            iconName={"delete"}
+            itemMenuName={"Eliminar lista"}
+            onClick={deleteLista}
         />
-      <ItemMenu
-        iconName={"check_box_outline_blank"}
-        itemMenuName={"Desmarcar todo"}
-        onClick={handleUnCheckAll}
-        style={{display: itemslength < 2 ? "none" : "flex"}}
-        />
-      <ItemMenu
-        iconName={lista.userConfig[usuario.uid].isArchived ? "unarchive" : "archive"}
-        itemMenuName={lista.userConfig[usuario.uid].isArchived ? "Desarchivar lista" : "Archivar lista"}
-        onClick={() => {handleArchive(lista);  navigate("/")}}
-      />
-      <ItemMenu
-        iconName={"edit"}
-        itemMenuName={"Editar lista"}
-        onClick={() => navigate(`/newlist?lista=${lista.id}`)}
-      />
-      <ItemMenu
-        iconName={"delete"}
-        itemMenuName={"Eliminar lista"}
-        onClick={deleteLista}
-      />
     </div>
   )
 })
